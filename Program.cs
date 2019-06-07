08f9ca970b33444708dab021accf80cf75b9def5
Mandar <mandardilse@gmail.com>
2019-06-08 00:02:09 +0530

    Insert comments

--- Program.cs
+++ Program.cs
@@ -2,19 +2,24 @@
 using System.Threading.Tasks;
 using DI.entities;
 using Microsoft.Extensions.DependencyInjection;
-
+//Use README instructions
 namespace DI
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			//Step 1
 			var serviceCollection = new ServiceCollection();
+			//for demo purpose, used actual single tone class and create instance.
 			var singleInstance = SingleToneClass.Instance;
 			System.Console.WriteLine($"Main method SingleTone Class Id: {singleInstance.GetHashCode().ToString()}");
+
 			serviceCollection.AddSingleton<Configuration>();
 			serviceCollection.AddScoped<ScopeClass>();
 			serviceCollection.AddTransient<TransientClass>();
+
+			//Step 2
 			IServiceProvider builder = serviceCollection.BuildServiceProvider();
 			System.Console.WriteLine($"Main method Scoped Class Id: {builder.GetService<ScopeClass>().GetHashCode().ToString()}");
 			System.Console.WriteLine($"Main method Configuration Class Id: {builder.GetService<Configuration>().GetHashCode().ToString()}");
@@ -28,6 +33,7 @@ namespace DI
 		static void InstantiateClasses(IServiceProvider builder)
 		{
 			var singleToneInstance = SingleToneClass.Instance;
+			//Step 3
 			System.Console.WriteLine($"SingleTone Class Id: {singleToneInstance.GetHashCode().ToString() }");
 			System.Console.WriteLine($"Scoped Class Id: {builder.GetService<ScopeClass>().GetHashCode().ToString()}");
 			System.Console.WriteLine($"Transient Class Id : { builder.GetService<TransientClass>().GetHashCode().ToString()}");
